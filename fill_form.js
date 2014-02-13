3dd9bdf85cbab5f9aa17847d6afe509109cf2587
Dick Fickling <dick@recurly.com>
2014-02-13 07:26:54 -0800

    Add icons, move regex to server

--- fill_form.js
+++ fill_form.js
@@ -1,12 +1,14 @@
 function requestIdentity(callback) {
   var xhr = new XMLHttpRequest();
 
-  xhr.open("GET", "http://fickling.us/rig", true);
+  xhr.open("GET", "http://randomautofill.com", true);
   xhr.onreadystatechange = function() {
     if (xhr.readyState === 4) {
       if (xhr.status == 200) {
-        var rig = JSON.parse(xhr.responseText);
-        callback(rig);
+        var response = JSON.parse(xhr.responseText);
+        var rig = response.rig;
+        var regexes = response.regexes;
+        callback(rig, regexes);
 
       } else {
         alert("Identity server is down");
@@ -17,7 +19,7 @@ function requestIdentity(callback) {
   xhr.send(null);
 }
 
-function checkRegex(element_name) {
+function checkRegex(element_name, regexes) {
   for (var regex_name in regexes) {
     var re = new RegExp(regexes[regex_name], "i");
     if (re.test(element_name)) return regex_name;
@@ -25,32 +27,18 @@ function checkRegex(element_name) {
   return null;
 }
 
-function autoFill(identity) {
+function autoFill(identity, regexes) {
   for(var form_id in document.forms) {
     var elements = document.forms[form_id].elements;
     for (var element_id in elements) {
       var element = elements[element_id];
-      var regexMatch = checkRegex(element.name);
+      var regexMatch = checkRegex(element.name, regexes);
       if (!regexMatch) continue;
 
       element.value = identity[regexMatch];
     }
       
   }
-}
-
-regexes = {
-  street_address: "address.*line|address1|addr1|street",
-  country: "country|countries",
-  zip: "zip|postal|post.*code|pcode",
-  city: "city|town",
-  state: "state",
-  email: "e.?mail",
-  first_name: "first.*name|fname|first",
-  last_name: "last.*name|lname|surname|last$|secondname",
-  phone: "phone|mobile|cell",
-  credit_card: "card.?number|card.?#|card.?no|cc.?num",
-  cvv: "verification|card identification|security code|cvn|cvv|cvc|csc"
 }
 
 requestIdentity(autoFill);
